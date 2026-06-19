64b2cf179c2293f0095cb9e8124c7b0df75ef772
adebrabant <22966649+adebrabant@users.noreply.github.com>
2026-06-18 20:26:24 -0400

    Adding MaterialManager & tests;

---
+++ Cocoa/include/Graphics/MaterialManager.hpp
@@ -0,0 +1,47 @@
+#pragma once
+
+#include "Graphics/Material.hpp"
+
+#include <string>
+#include <cstdint>
+#include <unordered_map>
+
+namespace Cocoa::Core
+{
+	class Color;
+}
+
+namespace Cocoa::Graphics
+{
+	struct TextureHandle;
+	struct ShaderHandle;
+
+	struct MaterialHandle
+	{
+		uint32_t Id;
+	};
+
+	class MaterialManager
+	{
+	public:
+		MaterialManager();
+		~MaterialManager();
+
+		MaterialManager(const MaterialManager&) = delete;
+		MaterialManager& operator=(const MaterialManager&) = delete;
+
+		MaterialHandle Load(
+			const std::string& materialId,
+			ShaderHandle shaderHandle,
+			TextureHandle textureHandle,
+			Core::Color tint
+		);
+
+		const Material& Get(MaterialHandle handle) const;
+
+	private:
+		std::unordered_map<std::string, MaterialHandle> m_handles;
+		std::unordered_map<uint32_t, Material> m_materials;
+		uint32_t m_nextId;
+	};
+}
