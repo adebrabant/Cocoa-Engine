a04adc2487044d782165fbb8e11848714697bf19
adebrabant <22966649+adebrabant@users.noreply.github.com>
2026-08-15 14:04:57 -0400

    Adding in scene testing with multiple quads

--- Sandbox/src/Scenes/TestScene.cpp
+++ Sandbox/src/Scenes/TestScene.cpp
@@ -22,17 +22,55 @@ namespace Sandbox
         m_world.AddComponent(cameraEntity, Cocoa::Scenes::CameraComponent{ 0, 0, 0, 60.0f });
         m_world.AddSystem<Cocoa::Scenes::CameraSystem>();
 
-        auto dummyEntity = m_world.CreateEntity();
-        auto materialHandle = loader.LoadMaterial("werewolf_material");
-    	Cocoa::Scenes::TransformComponent wereWolfTransform
-    	{
-    		.Position = { 0.2f, 0.0f, 0.0f },
-    		.Rotation = {0.0f, 0.0f, 0.0f },
-    		.Scale =  { 0.5f, 0.5f, 0.5f }
-    	};
-    	m_world.AddComponent(dummyEntity, wereWolfTransform);
-        m_world.AddComponent(dummyEntity, Cocoa::Scenes::Renderable2DComponent{ materialHandle });
-        m_world.AddSystem<Cocoa::Scenes::RenderSystem>();
+		auto sunEntity = m_world.CreateEntity();
+		auto sunMaterialHandle = loader.LoadMaterial("sunset_background_01_material");
+		Cocoa::Scenes::TransformComponent sunsetTransform
+		{
+			.Position = { 0.0f, 0.0f, 0.0f },
+			.Rotation =  { 0.0f, 0.0f, 0.0f },
+			.Scale = { 3.56f, 2.0f, 1.0f }
+		};
+
+		auto mountainBackgroundEntity = m_world.CreateEntity();
+		auto mountainBackgroundMaterialHandle = loader.LoadMaterial("mountains_background_01_material");
+		Cocoa::Scenes::TransformComponent mountainBackgroundTransform
+		{
+			.Position = { 0.0f, -0.05f, 0.0f },
+			.Rotation =  { 0.0f, 0.0f, 0.0f },
+			.Scale = { 3.56f, 2.0f, 1.0f }
+		};
+
+		auto forestFogBackground01Entity = m_world.CreateEntity();
+		auto forestFogBackground01MaterialHandle = loader.LoadMaterial("forest_fog_background_01_material");
+		Cocoa::Scenes::TransformComponent forestFogBackground01Transform
+		{
+			.Position = { 0.0f, -0.15f, 0.0f },
+			.Rotation =  { 0.0f, 0.0f, 0.0f },
+			.Scale = { 3.56f, 2.0f, 1.0f }
+		};
+
+		auto forestFogBackground02Entity = m_world.CreateEntity();
+		auto forestFogBackground02MaterialHandle = loader.LoadMaterial("forest_fog_background_02_material");
+		Cocoa::Scenes::TransformComponent forestFogBackground02Transform
+		{
+			.Position = { 0.0f, -0.25f, 0.0f },
+			.Rotation =  { 0.0f, 0.0f, 0.0f },
+			.Scale = { 3.56f, 2.0f, 1.0f }
+		};
+
+		m_world.AddComponent(sunEntity, sunsetTransform);
+		m_world.AddComponent(sunEntity, Cocoa::Scenes::Renderable2DComponent{ sunMaterialHandle });
+
+		m_world.AddComponent(mountainBackgroundEntity, mountainBackgroundTransform);
+		m_world.AddComponent(mountainBackgroundEntity, Cocoa::Scenes::Renderable2DComponent{ mountainBackgroundMaterialHandle });
+
+		m_world.AddComponent(forestFogBackground01Entity, forestFogBackground01Transform);
+		m_world.AddComponent(forestFogBackground01Entity, Cocoa::Scenes::Renderable2DComponent{ forestFogBackground01MaterialHandle });
+
+		m_world.AddComponent(forestFogBackground02Entity, forestFogBackground02Transform);
+		m_world.AddComponent(forestFogBackground02Entity, Cocoa::Scenes::Renderable2DComponent{ forestFogBackground02MaterialHandle });
+
+		m_world.AddSystem<Cocoa::Scenes::RenderSystem>();
     }
 
 	void TestScene::Unload(Cocoa::Assets::ResourceLoader& loader)
