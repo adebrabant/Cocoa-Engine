5d2262e21ef8a687dcfcc6affcaf2406216c1ef6
adebrabant <22966649+adebrabant@users.noreply.github.com>
2026-09-19 21:41:15 -0400

    Adding TilingFactor to Render Component

--- Cocoa/include/Scenes/Components/Renderable2DComponent.hpp
+++ Cocoa/include/Scenes/Components/Renderable2DComponent.hpp
@@ -6,7 +6,8 @@ namespace Cocoa::Scenes
 {
 	struct Renderable2DComponent
 	{
-		Graphics::MaterialHandle Material;
-		Graphics::SpriteHandle Sprite;
+		Graphics::MaterialHandle Material{};
+		Graphics::SpriteHandle Sprite{};
+		float TilingFactor{1.0f};
 	};
 }
